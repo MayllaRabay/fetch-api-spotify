101321ed6b41b7cc57261ff19762ba5aa458759d
MayllaRabay <mayllarabay@gmail.com>
2021-06-15 13:44:37 -0300

    Folders 'components' e 'utils' criados; estilizaçao feita; function 'handleSubmit' iniciada.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React, { useState } from 'react';
 import './App.css';
 
+const baseURL = (pesquisa) => `https://api.spotify.com/v1/search?q={pesquisa}&type=track&limit=10`;
+
 function App() {
+  const [ pesquisa, setPesquisa ] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if(!pesquisa) return;
+  }
+
   return (
     <div className="App">
-      
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={pesquisa} onChange={e => setPesquisa(e.target.value)} />
+      </form>
     </div>
   );
 }
